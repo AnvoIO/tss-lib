6949e39d8d14899560e794d5b81ce98547ebf78d
Robert <robert@cryptobloks.io>
2026-03-19 23:48:03 +0000

    style: fix import ordering in eddsa/signing after module re-path

--- eddsa/signing/finalize.go
+++ eddsa/signing/finalize.go
@@ -12,8 +12,8 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/agl/ed25519/edwards25519"
 	"github.com/AnvoIO/tss-lib/v3/tss"
+	"github.com/agl/ed25519/edwards25519"
 	"github.com/decred/dcrd/dcrec/edwards/v2"
 )
 
